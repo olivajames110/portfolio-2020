3b9d150cd2a0e2dba6ce84df0b26c8a66c410417
Jimmy Oliva <olivajames110@gmail.com>
2022-01-10 13:56:45 -0500

    Mobile

--- frontend/src/screens/AboutScreen/AboutScreen.js
+++ frontend/src/screens/AboutScreen/AboutScreen.js
@@ -12,25 +12,27 @@ const AboutScreen = () => {
       <PageHeaderImage image={AboutScreenHeaderImage} title="ABOUT ME" />
       <Main>
         <section id="about">
-          <div className="about__content">
-            <h2>About Myself</h2>
+          <div className="about-inner-wrapper">
+            <div className="about__content">
+              <h2>About Myself</h2>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              molestie, neque at efficitur molestie, ante turpis aliquam libero,
-              non congue massa dui et libero. Nulla viverra libero in nibh
-              semper, a cursus leo sodales.
-            </p>
-            <p>
-              Fusce ultrices urna purus, sit amet dictum leo pretium vel. Etiam
-              tristique sapien ut nisi vestibulum viverra. Fusce bibendum eros
-              in felis condimentum fermentum. Mauris enim odio, faucibus eget
-              sodales facilisis, gravida tristique mi. Donec finibus, metus ac
-              erat elit vulputate mauris sollicitudin, a convallis magna
-              vestibulum. Proin finibus ante sed ultrices fringilla. Donec enim
-              urna, lacinia ut rhoncus ac, pulvinar in nulla. Cras in sem ac
-              erat accumsan efficitur.
-            </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+                molestie, neque at efficitur molestie, ante turpis aliquam
+                libero, non congue massa dui et libero. Nulla viverra libero in
+                nibh semper, a cursus leo sodales.
+              </p>
+              <p>
+                Fusce ultrices urna purus, sit amet dictum leo pretium vel.
+                Etiam tristique sapien ut nisi vestibulum viverra. Fusce
+                bibendum eros in felis condimentum fermentum. Mauris enim odio,
+                faucibus eget sodales facilisis, gravida tristique mi. Donec
+                finibus, metus ac erat elit vulputate mauris sollicitudin, a
+                convallis magna vestibulum. Proin finibus ante sed ultrices
+                fringilla. Donec enim urna, lacinia ut rhoncus ac, pulvinar in
+                nulla. Cras in sem ac erat accumsan efficitur.
+              </p>
+            </div>
           </div>
           <div className="about__graphic">{aboutGraphic}</div>
         </section>
