d075b114f32653a56dd01b7ee805e6f167765c43
olivajames110 <olivajames110@gmail.com>
2022-05-05 15:31:47 -0400

    Device preview

--- frontend/src/screens/ProjectsScreen/DevicePreview/DevicePreview.js
+++ frontend/src/screens/ProjectsScreen/DevicePreview/DevicePreview.js
@@ -114,7 +114,7 @@ const DevicePreviewOverlay = (props) => {
                   !previewIsMobile && "icon-active"
                 }`}
               >
-                <Desktop />
+                {desktop}
               </div>
               <div
                 onClick={mobilePreviewHandler}
@@ -122,7 +122,7 @@ const DevicePreviewOverlay = (props) => {
                   previewIsMobile && "icon-active"
                 }`}
               >
-                <Mobile />
+                {mobileIcon}
               </div>
             </React.Fragment>
           )}
@@ -172,3 +172,37 @@ const DevicePreview = (props) => {
 };
 
 export default DevicePreview;
+
+const mobileIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={1}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"
+    />
+  </svg>
+);
+
+const desktop = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={1}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+    />
+  </svg>
+);
